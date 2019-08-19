4ed3a4eb387ec8f4e95d041fe5e58be4decef0e5
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-19 12:20:26 -0500

    Update test.py
    Fix os.path.dirname()

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from H2O_getNLCD import getNLCD
 
 # Purpose: script to get NLCD rasters for bounding box
 AOI = r"C:\ArcGIS\Local_GIS\H2O\AOI\Monroe_Parish_3857.shp"
-directory = os.path.dir(AOI)
+directory = os.path.dirname(AOI)
 year = "2016"
 dataset = "Land_Cover"
 
