0466e7963467712d7abd4cf3e97961fe64997c91
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-19 14:35:51 -0500

    updated to take example aoi shapefile

--- test/test.py
+++ test/test.py
@@ -1,10 +1,18 @@
 import os
+import geopandas
 from H2O_getNLCD import getNLCD
 
-# Purpose: script to get NLCD rasters for bounding box
-AOI = r"C:\ArcGIS\Local_GIS\H2O\AOI\Monroe_Parish_3857.shp"
-directory = os.path.dirname(AOI)
+# Purpose: Test script to get NLCD rasters for bounding box
+
+# Example shapefile
+filepath =  os.path.realpath(os.path.join(os.getcwd(),
+                                          os.path.dirname(__file__)))
+shp = os.path.join(filepath, "example_shp/example_AOI.shp"
+
+# Read example shapefile to geopandas dataframe
+gpd_df = geopandas.read_file(shp)
+
 year = "2016"
 dataset = "Land_Cover"
 
-getNLCD(AOI, directory, dataset, year)
+getNLCD(gpd_df, filepath, dataset, year)
