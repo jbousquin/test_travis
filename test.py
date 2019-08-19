ec0d98455b9c7b8ac6f6687622e4942660fdac0b
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-19 10:47:16 -0500

    test getNLCD()

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-import os
-from utils import message
+import H2O_getNLCd
 
-message("Hello world")
+# Purpose: script to get NLCD rasters for bounding box
+AOI = r"C:\ArcGIS\Local_GIS\H2O\AOI\Monroe_Parish_3857.shp"
+directory = os.path.dir(AOI)
+year = "2016"
+dataset = "Land_Cover"
+
+getNLCD(AOI, directory, dataset, year)
