e66c5445d8c1e77fcbd1862be3397d135a018b31
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-20 16:03:09 -0500

    Initial commit

---
+++ H2O_getNLCD/getFIPs.py
@@ -0,0 +1,70 @@
+import pyproj
+import geopandas
+from json import loads
+
+
+def py_prj(shp):
+    """Return EPSG for shapefile"""
+    #shp = geopandas.read_file(fc)
+    return shp.crs['init'][5:]
+
+
+def py_getBoundingBox(shp):
+    """Use geopandas library instead of arcpy
+    param@shp should be a geopandas dataframe object
+    """
+    #shp = geopandas.read_file(fc)
+    xmin = shp.bounds['minx'][0]
+    xmax = shp.bounds['maxx'][0]
+    ymin = shp.bounds['miny'][0]
+    ymax = shp.bounds['maxy'][0]
+
+    return [xmin, ymin, xmax, ymax]
+
+
+def geoQuery(geo, inSR, fields, geoType = "esriGeometryEnvelope"):
+    """ Return query as formatted dicitonary"""
+    data = {"geometry": geo,
+            "geometryType": geoType,
+            "inSR": inSR,
+            "spatialRel": "esriSpatialRelIntersects",
+            "returnGeometry": "false",
+            "outFields": ','.join(fields),
+            }
+    return data
+
+
+def mapServerRequest(catalog, service, layer, query, server = "arcgis",
+                     typ = "MapServer", token = ""):
+    """send query to map server using requests"""
+    url = "https://{}/{}/rest/services/{}/{}/".format(catalog, server, service, typ)
+    if layer is not None:
+        req += "{}/".format(layer)
+    # Return json
+    query['f'] = 'json'
+    if token:
+        # This won't actually work as written.
+        query["token"] = token
+    try:
+        res = requests.get(url, query)
+        assert res.ok
+        return loads(res.content)
+    except Exception as exc:
+        message("Could not get a response using: " + url + query)
+        raise
+
+
+def polyFIPS(poly):
+    """Returns county FIPs intersecting polygon extent"""
+    envelope = ','.join(py_getBoundingBox(poly)) # Get extent
+    inSR = py_prj(poly) # Get spatial reference
+    fields = ["NAME", "GEOID"]
+    query = geoQuery(envelope, inSR, fields)
+    catalog = "tigerweb.geo.census.gov"
+    service = "TIGERweb/tigerWMS_Current"
+    layer = 86 #Counties ID: 86
+    resultJSON = MapServerRequest(catalog, service, layer, query)
+    #counties = [c['attributes'][fields[0]] for c in resultJSON['features']]
+    FIPS = [c['attributes'][fields[1]] for c in resultJSON['features']]
+
+    return FIPS
---
+++ H2O_getNLCD/getRoads.py
@@ -0,0 +1,70 @@
+import requests
+import geopandas
+from os.path import join
+from zipfile import ZipFile
+
+
+def message(string):
+    print(string)
+
+
+def checkYear(year):
+    """Run checks and formatting on year"""
+    year = str(year) # coerce to string in case float() or int()
+    assert year.isdigit(), "The year parameter must be numeric"
+    return year
+
+
+def getArchive(response):
+    from StringIO import StringIO #only used if no directory
+    # Check zip is valid?
+    #assert zipfile.is_zipfile(StringIO(response.content))
+    # Unpack zip
+    with ZipFile(StringIO(response.content), 'r') as archive:
+        contents = archive.namelist()
+        for f in contents:
+            if f.endswith(".shp"):
+                return geopandas.read_file(f)
+
+
+def roads(FIP, directory = None, year = "2019"):
+    """Download shapefile road lines by county
+    """
+    # Make sure year is usable
+    year = checkYear(year)
+    if int(year) <= 2008:
+        years = ["1992", "1999", "2002", "2003",]
+        assert year in years, "The year {} is not available".format(year)
+    else:
+        assert int(year) < 2020, "Only available through 2019"
+
+    # Make sure FIP is expected length
+    assert len(FIP) == 5, "Problem with specified FIP: {}".format(FIP)
+
+    # Source url
+    url = "https://www2.census.gov/geo/tiger/TIGER{}/ROADS/".format(year)
+
+    # Create file name for county
+    download = "tl_{}_{}_roads.zip".format(year, FIP)
+
+    # Download zip file (stream = True may work better for larger files)
+    res = requests.get(url + download)
+    assert res.ok, "Problem with response from {}".format(url + download)
+
+    # Save zip to directory if specified
+    if directory != None:
+        out_file = join(directory, download)
+        with open(out_file, "wb") as f:
+            f.write(res.content)
+
+        # Check & unpack archive
+        assert checkArchive(out_file), "Bad zipfile"
+        with ZipFile(out_file) as archive:
+            archive.extractall(directory)
+        shp = download[:-3] + "shp"
+        df = geopandas.read_file(join(directory, shp))
+
+    else:
+        df = getArchive(res)
+        
+    return df
