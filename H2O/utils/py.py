ee3fae69adcade548180530b15a3729d0aa53704
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-21 19:34:30 -0500

    started adding raster processing (clip by poly, summarize by poly)

--- H2O/utils/py.py
+++ H2O/utils/py.py
@@ -42,3 +42,28 @@ def transform_bBox(bBox, inEPSG, outEPSG):
     pnt2_out = transform_pnt(pnt2, inEPSG, outEPSG)
 
     return [pnt1_out[0], pnt2_out[0], pnt1_out[1], pnt2_out[1]]
+
+
+#def clipRaster(raster, poly):
+#"""Clip raster down to polygon geometry
+#"""
+#    import rasterio
+#    import rasterio.mask
+#    features = poly["geometry"]
+#    with rasterio.open(raster) as src:
+#        out_image, out_transform = rasterio.mask.mask(src, features, crop=True)
+#        out_meta = src.meta.copy()
+#
+#    out_meta.update({"driver": "GTiff",
+#                     "height": out_image.shape[1],
+#                     "transform": out_transform})
+#    with rasterio.open(raster, "w", **out_meta) as src_out:
+#        src.write(out_image)
+#
+#
+#def summarizeNLCD(poly, raster):
+#"""Summarize raster land uses classes by percent in polygon geometry
+#"""
+#    import rasterstats
+#
+#    zonal = rasterstats.zonal_stats()
