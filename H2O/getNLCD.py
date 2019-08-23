e468f4aa7091e43209840c7ae49b7dc4728da54a
Justin Bousquin <bousquin.justin@epa.gov>
2019-08-23 16:37:06 -0500

    added what dataset response had an issue to assert

--- H2O/getNLCD.py
+++ H2O/getNLCD.py
@@ -59,7 +59,7 @@ def getNLCD(poly, directory = None, dataset = "Land_Cover", year = "2016"):
             }
     # Get response
     res = requests.get(url, data)
-    assert res.ok, "Problem with response from: {}".format(strReq(url, data))
+    assert res.ok, "Problem with NLCD response: {}".format(strReq(url, data))
 
     # Write response to directory if provided
     if directory != None:    
